682af7e59e48d7444162e72213f24b43dde2010a
Dheeraj <dheerajshyamlalnagpal@gmail.com>
2025-04-08 13:11:27 -0400

    Added support to read resources from MCP SSE Server

--- src/multiserver_mcp_client.py
+++ src/multiserver_mcp_client.py
@@ -1,6 +1,18 @@
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langgraph.prebuilt import create_react_agent
 from my_mcp_clent import get_llm_model, print_messages
+from mcp.client.sse import sse_client
+from mcp.client.session import ClientSession
+from mcp.types import (
+    EmptyResult,
+    ErrorData,
+    InitializeResult,
+    ReadResourceResult,
+    TextContent,
+    TextResourceContents,
+    Tool,
+)
+from pydantic import AnyUrl
 
 async def run(model):
     async with MultiServerMCPClient(
@@ -17,10 +29,10 @@ async def run(model):
             }
         }
     ) as client:
+        print(client)
         agent = create_react_agent(get_llm_model(model), client.get_tools())
         math_response = await agent.ainvoke({"messages": "what's (3 + 5) x 12?"})
         all_notes_response = await agent.ainvoke({"messages": "List all notes"})
-        
 
         print("=== EXECUTION PATH ===")
         # Extract the messages from the response
@@ -29,6 +41,17 @@ async def run(model):
 
         messages = all_notes_response.get('messages', [])
         print_messages(messages)
+    
+    print("Resource with Note ID 2:")
+    uri = "note://2"
+    print(f"URI: {uri}")
+    async with sse_client("http://localhost:3000/sse") as streams:
+        async with ClientSession(*streams) as session:
+            await session.initialize()
+            resource = await session.read_resource(uri=AnyUrl(uri))
+            text_content = resource.contents[0]
+            if isinstance(text_content, TextResourceContents):
+                print(f"Note content: {text_content.text}")
 
 if __name__ == "__main__":
     import asyncio
